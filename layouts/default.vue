30a4d3a9d6e7e2222a52298b118cf5166b66b0d5
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-07-22 14:15:49 +0700

    add kota.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,13 +12,30 @@
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
+        <!-- <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item to="/provinsi">Provinsi</b-nav-item>
             <b-nav-item to="/kabupaten">Kabupaten</b-nav-item>
             <b-nav-item to="/desa">Desa</b-nav-item>
           </b-navbar-nav>
-        </b-collapse>
+        </b-collapse> -->
+        <div>
+          <b-nav pills>
+            <b-nav-item-dropdown
+              id="my-nav-dropdown"
+              text="Data"
+              toggle-class="nav-link-custom"
+              right
+            >
+              <b-dropdown-item to="/provinsi">Provinsi</b-dropdown-item>
+              <b-dropdown-item to="/kabupaten">Kabupaten</b-dropdown-item>
+              <b-dropdown-item to="/kota">Kota</b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>Three</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/desa">Desa</b-nav-item>
+          </b-nav>
+        </div>
       </b-navbar>
     </div>
 

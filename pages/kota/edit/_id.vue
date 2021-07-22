2a74559240b7dd0d3632b2cf9d42574e07e7a8f4
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-07-22 20:18:53 +0700

    add edit

--- pages/kota/edit/_id.vue
+++ pages/kota/edit/_id.vue
@@ -3,38 +3,38 @@
     <b-row>
       <b-col md="12">
         <b-card class="shadow-md border-0 rounded-lg">
-          <h5>EDIT POST</h5>
+          <h5>Edit Kota</h5>
           <hr />
           <b-form @submit="update">
-            <b-form-group label="Provinsi">
+            <b-form-group label="Kota">
               <b-form-select
-                v-model="kabupaten.id_provinsi"
+                v-model="kota.id_provinsi"
                 :options="provinsi"
                 value-field="id"
                 text-field="provinsi"
               ></b-form-select>
-              <div v-if="validation.kabupaten" class="mt-2">
+              <div v-if="validation.kota" class="mt-2">
                 <b-alert show variant="danger">{{
-                  validation.kabupaten[0]
+                  validation.kota[0]
                 }}</b-alert>
               </div>
             </b-form-group>
-            <b-form-group label="Kabupaten">
+            <b-form-group label="Kota">
               <b-form-input
                 type="text"
-                v-model="kabupaten.kabupaten"
+                v-model="kota.kota"
                 :class="{ 'is-invalid': validation.title }"
-                placeholder="Masukkan Kabupaten"
+                placeholder="Masukkan Kota"
               >
               </b-form-input>
-              <div v-if="validation.kabupaten" class="mt-2">
+              <div v-if="validation.kota" class="mt-2">
                 <b-alert show variant="danger">{{
-                  validation.kabupaten[1]
+                  validation.kota[1]
                 }}</b-alert>
               </div>
             </b-form-group>
 
-            <b-button type="submit" variant="primary">UPDATE</b-button>
+            <b-button type="submit" variant="primary">Update</b-button>
           </b-form>
         </b-card>
       </b-col>
@@ -48,9 +48,9 @@ export default {
     return {
       //state post
       provinsi: [],
-      kabupaten: {
+      kota: {
         id_provinsi: "",
-        kabupaten: ""
+        kota: ""
       },
       //state validation
       validation: []
@@ -60,38 +60,35 @@ export default {
   mounted() {
     console.log(this.$route.params.id);
     //get data post by ID
-    this.$axios
-      .get(`/api/kabupaten/${this.$route.params.id}`)
-      .then(response => {
-        (this.kabupaten.id_provinsi = response.data.data.id_provinsi),
-          (this.kabupaten.kabupaten = response.data.data.kabupaten),
-          this.$axios
-            .get("/api/provinsi")
-            .then(response => {
-              //assign response ke state "provinsi"
-              this.provinsi = response.data.data;
-            })
-            .catch(error => {
-              console.log(error.response.data);
-            });
-      });
+    this.$axios.get(`/api/kota/${this.$route.params.id}`).then(response => {
+      (this.kota.id_provinsi = response.data.data.id_provinsi),
+        (this.kota.kota = response.data.data.kota),
+        this.$axios
+          .get("/api/provinsi")
+          .then(response => {
+            //assign response ke state "kota"
+            this.provinsi = response.data.data;
+          })
+          .catch(error => {
+            console.log(error.response.data);
+          });
+    });
   },
 
   methods: {
     async update(e) {
       e.preventDefault();
-
       //send data ke Rest API untuk update
       await this.$axios
-        .put(`/api/kabupaten/${this.$route.params.id}`, {
+        .put(`/api/kota/${this.$route.params.id}`, {
           //data yang dikirim
-          id_provinsi: this.kabupaten.id_provinsi,
-          kabupaten: this.kabupaten.kabupaten
+          id_provinsi: this.kota.id_provinsi,
+          kota: this.kota.kota
         })
         .then(() => {
           //redirect ke route "post"
           this.$router.push({
-            name: "kabupaten"
+            name: "kota"
           });
         })
         .catch(error => {
